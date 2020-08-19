830576b8ef81199626231e1d021a13384ac9b306
Karthik-B-06 <karthik06051996@gmail.com>
2020-08-20 00:14:09 +0530

    fix: :rotating_light: Fixed prop type shadowStyle is invalid warning

--- examples/RNSegmentedControl/App.js
+++ examples/RNSegmentedControl/App.js
@@ -6,68 +6,11 @@
  * @flow strict-local
  */
 
-import React from 'react';
-import {
-  SafeAreaView,
-  StyleSheet,
-  ScrollView,
-  View,
-  Text,
-  StatusBar,
-} from 'react-native';
+import React from "react";
+import AppRoot from "./src/appRoot";
 
-import {
-  Header,
-  LearnMoreLinks,
-  Colors,
-  DebugInstructions,
-  ReloadInstructions,
-} from 'react-native/Libraries/NewAppScreen';
-import AppRoot from './src/appRoot';
-
-const App: () => React$Node = () => {
-  return (
-    <AppRoot />
-  );
+const App = () => {
+  return <AppRoot />;
 };
-
-const styles = StyleSheet.create({
-  scrollView: {
-    backgroundColor: Colors.lighter,
-  },
-  engine: {
-    position: 'absolute',
-    right: 0,
-  },
-  body: {
-    backgroundColor: Colors.white,
-  },
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
-  },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: '600',
-    color: Colors.black,
-  },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
-    color: Colors.dark,
-  },
-  highlight: {
-    fontWeight: '700',
-  },
-  footer: {
-    color: Colors.dark,
-    fontSize: 12,
-    fontWeight: '600',
-    padding: 4,
-    paddingRight: 12,
-    textAlign: 'right',
-  },
-});
 
 export default App;
--- examples/RNSegmentedControl/src/components/segmentedControl/index.js
+++ examples/RNSegmentedControl/src/components/segmentedControl/index.js
@@ -1,24 +1,31 @@
-import PropTypes from 'prop-types';
-import React, { useEffect } from 'react';
-import { Animated, Dimensions, StyleSheet, Text, TouchableOpacity, ViewPropTypes } from 'react-native';
+import PropTypes from "prop-types";
+import React, { useEffect } from "react";
+import {
+  Animated,
+  Dimensions,
+  StyleSheet,
+  Text,
+  TouchableOpacity,
+  ViewPropTypes,
+} from "react-native";
 
 const getSegmentedBackgroundColor = (theme, colorValueFromProps) => {
-  return colorValueFromProps || (theme === 'LIGHT' ? '#E5E5EA' : '#4a5568');
-}
+  return colorValueFromProps || (theme === "LIGHT" ? "#E5E5EA" : "#4a5568");
+};
 
 const getSegmentedTextColor = (theme, colorValueFromProps) => {
-  return colorValueFromProps || (theme === 'LIGHT' ? 'black' : 'white');
-}
+  return colorValueFromProps || (theme === "LIGHT" ? "black" : "white");
+};
 
 const getActiveSegmentedBackgroundColor = (theme, colorValueFromProps) => {
-  return colorValueFromProps || (theme === 'LIGHT' ? 'white' : 'black');
-}
+  return colorValueFromProps || (theme === "LIGHT" ? "white" : "black");
+};
 
 const getActiveSegmentedTextColor = (theme, colorValueFromProps) => {
-  return colorValueFromProps || (theme === 'LIGHT' ? 'black' : 'white');
-}
+  return colorValueFromProps || (theme === "LIGHT" ? "black" : "white");
+};
 
-const shadow = {
+const defaultShadowStyle = {
   shadowColor: "#000",
   shadowOffset: {
     width: 0,
@@ -28,14 +35,13 @@ const shadow = {
   shadowRadius: 2.62,
 
   elevation: 4,
-}
-
+};
 
 const SegmentedControl = (props) => {
-  const { width } = props;
-  const translateValue = ((width - 4) / props?.tabs?.length);
+  const { width, shadowStyle } = props;
+  const translateValue = (width - 4) / props?.tabs?.length;
   const [tabTranslate, setTabTranslate] = React.useState(new Animated.Value(0));
-
+  const shadow = shadowStyle || defaultShadowStyle;
   // useCallBack with an empty array as input, which will call inner lambda only once and memoize the reference for future calls
   const memoizedTabPressCallback = React.useCallback(
     (index) => {
@@ -54,95 +60,111 @@ const SegmentedControl = (props) => {
       stiffness: 150,
       damping: 20,
       mass: 1,
-      useNativeDriver: true
-    }).start()
-  }, [props?.currentIndex])
+      useNativeDriver: true,
+    }).start();
+  }, [props?.currentIndex]);
 
   return (
-    <Animated.View style={[
-      props?.containerStyle,
-      styles.segmentedControlWrapper,
-      {
-        width: width,
-      },
-      {
-        backgroundColor: getSegmentedBackgroundColor(props?.theme, props?.segmentedControlBackgroundColor)
-      },
-      {
-        paddingVertical: props?.paddingVertical,
-      },
-    ]}>
-      <Animated.View
-        style={[{
-          ...StyleSheet.absoluteFill,
-          position: "absolute",
-          width: (width - 4) / props?.tabs?.length,
-          top: 0,
-          marginVertical: 2,
-          marginHorizontal: 2,
-          backgroundColor: getActiveSegmentedBackgroundColor(props?.theme, props?.activeSegmentBackgroundColor),
-          borderRadius: 8,
-          ...shadow,
+    <Animated.View
+      style={[
+        props?.containerStyle,
+        styles.segmentedControlWrapper,
+        {
+          width: width,
         },
         {
-          transform: [
-            {
-              translateX: tabTranslate
-            }
-          ]
-        }]}
-      >
-      </Animated.View>
-      {
-        props?.tabs.map((tab, index) => {
-          const isCurrentIndex = props?.currentIndex === index;
-          return (
-            <TouchableOpacity
-              key={index}
-              style={[styles.textWrapper]}
-              onPress={() => memoizedTabPressCallback(index)}
-              activeOpacity={0.7} >
-              <Text
-                numberOfLines={1}
-                style={[
-                  styles.textStyles,
-                  props?.textStyle,
-                  isCurrentIndex ? {
-                    color: getActiveSegmentedTextColor(props?.theme, props?.activeTextColor)
-                  } : {
-                      color: getSegmentedTextColor(props?.theme, props?.textColor)
+          backgroundColor: getSegmentedBackgroundColor(
+            props?.theme,
+            props?.segmentedControlBackgroundColor
+          ),
+        },
+        {
+          paddingVertical: props?.paddingVertical,
+        },
+      ]}
+    >
+      <Animated.View
+        style={[
+          {
+            ...StyleSheet.absoluteFill,
+            position: "absolute",
+            width: (width - 4) / props?.tabs?.length,
+            top: 0,
+            marginVertical: 2,
+            marginHorizontal: 2,
+            backgroundColor: getActiveSegmentedBackgroundColor(
+              props?.theme,
+              props?.activeSegmentBackgroundColor
+            ),
+            borderRadius: 8,
+            ...shadow,
+          },
+          {
+            transform: [
+              {
+                translateX: tabTranslate,
+              },
+            ],
+          },
+        ]}
+      ></Animated.View>
+      {props?.tabs.map((tab, index) => {
+        const isCurrentIndex = props?.currentIndex === index;
+        return (
+          <TouchableOpacity
+            key={index}
+            style={[styles.textWrapper]}
+            onPress={() => memoizedTabPressCallback(index)}
+            activeOpacity={0.7}
+          >
+            <Text
+              numberOfLines={1}
+              style={[
+                styles.textStyles,
+                props?.textStyle,
+                isCurrentIndex
+                  ? {
+                      color: getActiveSegmentedTextColor(
+                        props?.theme,
+                        props?.activeTextColor
+                      ),
+                      fontWeight: props?.activeTextWeight,
+                    }
+                  : {
+                      color: getSegmentedTextColor(
+                        props?.theme,
+                        props?.textColor
+                      ),
                     },
-                ]}>
-                {tab}
-              </Text>
-            </TouchableOpacity>
-          )
-        })
-      }
+              ]}
+            >
+              {tab}
+            </Text>
+          </TouchableOpacity>
+        );
+      })}
     </Animated.View>
-  )
-}
-
+  );
+};
 
 const styles = StyleSheet.create({
   segmentedControlWrapper: {
-    display: 'flex',
-    flexDirection: 'row',
-    alignItems: 'center',
+    display: "flex",
+    flexDirection: "row",
+    alignItems: "center",
     borderRadius: 8,
   },
   textWrapper: {
     flex: 1,
     elevation: 9,
-    paddingHorizontal: 5
+    paddingHorizontal: 5,
   },
   textStyles: {
     fontSize: 18,
-    textAlign: 'center',
-    fontWeight: '600',
-  }
-})
-
+    textAlign: "center",
+    fontWeight: "600",
+  },
+});
 
 SegmentedControl.propTypes = {
   tabs: PropTypes.arrayOf(PropTypes.string).isRequired,
@@ -152,29 +174,41 @@ SegmentedControl.propTypes = {
   activeSegmentBackgroundColor: PropTypes.string,
   textColor: PropTypes.string,
   activeTextColor: PropTypes.string,
+  activeTextWeight: PropTypes.string,
   paddingVertical: PropTypes.number,
   width: PropTypes.number,
   containerStyle: ViewPropTypes.style,
   textStyle: PropTypes.object,
   isRTL: PropTypes.bool,
-  theme: PropTypes.oneOf(['LIGHT', 'DARK'])
-}
-
+  theme: PropTypes.oneOf(["LIGHT", "DARK"]),
+  shadowStyle: PropTypes.shape({
+    shadowColor: PropTypes.string,
+    shadowOffset: PropTypes.shape({
+      width: PropTypes.number,
+      height: PropTypes.number,
+    }),
+    shadowOpacity: PropTypes.number,
+    shadowRadius: PropTypes.number,
+    elevation: PropTypes.number,
+  }),
+};
 
 SegmentedControl.defaultProps = {
   tabs: [],
-  onChange: () => { },
+  onChange: () => {},
   currentIndex: 0,
   segmentedControlBackgroundColor: null,
   activeSegmentBackgroundColor: null,
   textColor: null,
   activeTextColor: null,
+  activeTextWeight: "600",
   paddingVertical: 12,
-  width: Dimensions.get('screen').width - 32,
+  width: Dimensions.get("screen").width - 32,
   containerStyle: {},
   textStyle: {},
   isRTL: false,
-  theme: 'LIGHT'
-}
+  theme: "LIGHT",
+  shadowStyle: null,
+};
 
 export default SegmentedControl;
