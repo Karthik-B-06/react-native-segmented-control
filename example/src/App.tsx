ad7c60a4c3f1fc0e2b17014346ac8e3b6f98d7ba
Karthik-B-06 <karthik06051996@gmail.com>
2022-01-15 17:49:45 +0530

    chore: :sparkles: updated example project with released library

--- example/src/App.tsx
+++ example/src/App.tsx
@@ -1,6 +1,6 @@
 import * as React from 'react';
 import { StyleSheet, View } from 'react-native';
-import SegmentedControl from './SegmentedControl';
+import SegmentedControl from 'rn-segmented-control';
 
 export default function App() {
   const [tabIndex, setTabIndex] = React.useState(0);
