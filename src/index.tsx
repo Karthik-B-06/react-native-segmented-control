2dbd82a102b610cdbbd22691603262cb60bffcc5
Karthik-B-06 <karthik06051996@gmail.com>
2022-01-15 16:42:53 +0530

    chore: :sparkles: wrapped basic Segmented Control component

--- src/index.tsx
+++ src/index.tsx
@@ -1,17 +1,31 @@
 import React, { useEffect } from 'react';
 import {
+  Pressable,
   StyleSheet,
   Text,
   TextStyle,
-  TouchableWithoutFeedback,
+  View,
+  ViewStyle,
 } from 'react-native';
-import Animated from 'react-native-reanimated';
+import Animated, {
+  useAnimatedStyle,
+  useSharedValue,
+  withSpring,
+} from 'react-native-reanimated';
 import { widthPercentageToDP } from 'react-native-responsive-screen';
 
-export interface SegmentedControlProps {
-  segments: string[];
-  onChange: (index: number) => void;
+interface SegmentedControlProps {
+  segments: Array<string>;
   currentIndex: number;
+  onChange: (index: number) => void;
+  badgeCount?: Array<number>;
+  isRTL?: boolean;
+  containerMargin?: number;
+  activeTextStyle?: TextStyle;
+  inactiveTextStyle?: TextStyle;
+  segmentedControlWrapper?: ViewStyle;
+  pressableWrapper?: ViewStyle;
+  tileStyle?: ViewStyle;
 }
 
 const defaultShadowStyle = {
@@ -26,14 +40,33 @@ const defaultShadowStyle = {
   elevation: 4,
 };
 
+const DEFAULT_SPRING_CONFIG = {
+  stiffness: 150,
+  damping: 20,
+  mass: 1,
+  overshootClamping: false,
+  restSpeedThreshold: 0.001,
+  restDisplacementThreshold: 0.001,
+};
+
 const SegmentedControl: React.FC<SegmentedControlProps> = ({
   segments,
   currentIndex,
   onChange,
+  badgeCount = [],
+  isRTL = false,
+  containerMargin = 0,
+  activeTextStyle,
+  inactiveTextStyle,
+  segmentedControlWrapper,
+  pressableWrapper,
+  tileStyle,
 }: SegmentedControlProps) => {
-  const width = widthPercentageToDP('100%') - 32;
+  const width = widthPercentageToDP('100%') - containerMargin * 2;
   const translateValue = width / segments.length;
-  const [tabTranslate] = React.useState(new Animated.Value(0));
+  const tabTranslateValue = useSharedValue(0);
+
+  // useCallBack with an empty array as input, which will call inner lambda only once and memoize the reference for future calls
   const memoizedTabPressCallback = React.useCallback(
     (index) => {
       onChange(index);
@@ -41,63 +74,100 @@ const SegmentedControl: React.FC<SegmentedControlProps> = ({
     [onChange]
   );
   useEffect(() => {
-    Animated.spring(tabTranslate, {
-      toValue: currentIndex * translateValue,
-      stiffness: 150,
-      damping: 20,
-      mass: 1,
-      overshootClamping: false,
-      restSpeedThreshold: 0.001,
-      restDisplacementThreshold: 0.001,
-    }).start();
-  }, [currentIndex, tabTranslate, translateValue]);
+    // If phone is set to RTL, make sure the animation does the correct transition.
+    const transitionMultiplier = isRTL ? -1 : 1;
+    tabTranslateValue.value = withSpring(
+      currentIndex * (translateValue * transitionMultiplier),
+      DEFAULT_SPRING_CONFIG
+    );
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [currentIndex]);
 
-  const activeTextStyle: TextStyle = {
+  const tabTranslateAnimatedStyles = useAnimatedStyle(() => {
+    return {
+      transform: [{ translateX: tabTranslateValue.value }],
+    };
+  });
+
+  const finalisedActiveTextStyle: TextStyle = {
     fontSize: 15,
     fontWeight: '500',
-    color: '#2563eb',
     textAlign: 'center',
+    color: '#0ea5e9',
+    ...activeTextStyle,
   };
-  const inActiveTextStyle: TextStyle = {
+
+  const finalisedInActiveTextStyle: TextStyle = {
     fontSize: 15,
-    color: '#2563eb',
     textAlign: 'center',
+    color: '#1f2937',
+    ...inactiveTextStyle,
+  };
+
+  const finalisedActiveBadgeStyle: ViewStyle = {
+    backgroundColor: '#0ea5e9',
+  };
+
+  const finalisedInActiveBadgeStyle: ViewStyle = {
+    backgroundColor: '#6b7280',
+  };
+
+  const finalisedBadgeTextStyle: TextStyle = {
+    fontSize: 11,
+    fontWeight: '500',
+    textAlign: 'center',
+    color: '#FFFFF',
   };
 
   return (
-    <Animated.View style={styles.segmentedControlWrapper}>
+    <Animated.View
+      style={[styles.defaultSegmentedControlWrapper, segmentedControlWrapper]}
+    >
       <Animated.View
         style={[
           styles.movingSegmentStyle,
+          defaultShadowStyle,
+          tileStyle,
           StyleSheet.absoluteFill,
           {
-            width: width / segments.length,
-            ...defaultShadowStyle,
+            width: width / segments.length - 4,
           },
-          {
-            transform: [
-              {
-                translateX: tabTranslate,
-              },
-            ],
-          },
+          tabTranslateAnimatedStyles,
         ]}
       />
       {segments.map((segment, index) => {
         return (
-          <TouchableWithoutFeedback
+          <Pressable
             onPress={() => memoizedTabPressCallback(index)}
-            key={segment}
-            style={[styles.textWrapper]}
+            key={index}
+            style={[styles.touchableContainer, pressableWrapper]}
           >
-            <Text
-              style={[
-                currentIndex === index ? activeTextStyle : inActiveTextStyle,
-              ]}
-            >
-              {segment}
-            </Text>
-          </TouchableWithoutFeedback>
+            <View style={styles.textWrapper}>
+              <Text
+                style={[
+                  currentIndex === index
+                    ? finalisedActiveTextStyle
+                    : finalisedInActiveTextStyle,
+                ]}
+              >
+                {segment}
+              </Text>
+              {badgeCount[index] > 0 && (
+                <View
+                  style={[
+                    styles.defaultBadgeContainerStyle,
+                    currentIndex === index
+                      ? finalisedActiveBadgeStyle
+                      : finalisedInActiveBadgeStyle,
+                  ]}
+                >
+                  <Text style={finalisedBadgeTextStyle}>
+                    {badgeCount[index]}
+                  </Text>
+                </View>
+              )}
+            </View>
+          </Pressable>
         );
       })}
     </Animated.View>
@@ -105,26 +175,39 @@ const SegmentedControl: React.FC<SegmentedControlProps> = ({
 };
 
 const styles = StyleSheet.create({
-  segmentedControlWrapper: {
+  defaultSegmentedControlWrapper: {
+    position: 'relative',
     display: 'flex',
     flexDirection: 'row',
     alignItems: 'center',
     borderRadius: 8,
-    backgroundColor: '#27272A',
-    paddingVertical: 10,
+    backgroundColor: '#f3f4f6',
   },
-  textWrapper: {
+  touchableContainer: {
     flex: 1,
     elevation: 9,
-    paddingHorizontal: 5,
+    paddingVertical: 12,
+  },
+  textWrapper: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'center',
   },
   movingSegmentStyle: {
-    position: 'absolute',
     top: 0,
     marginVertical: 2,
     marginHorizontal: 2,
-    borderRadius: 8,
+    borderRadius: 6,
     backgroundColor: '#FFFFFF',
+  },
+  // Badge Styles
+  defaultBadgeContainerStyle: {
+    alignItems: 'center',
+    justifyContent: 'center',
+    height: 16,
+    width: 16,
+    borderRadius: 9999,
+    alignContent: 'flex-end',
   },
 });
 
